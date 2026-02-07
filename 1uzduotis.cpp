78604b8ce74b0719e50fa2a94e8a03def1c011c3
egermanas34-beep <egermanas34@gmail.com>
2026-02-07 12:55:10 +0200

    Add files via upload
    pirmas uploadas

---
+++ 1uzduotis.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+//using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::vector;
+using std::fixed;
+using std::setprecision;
+struct Studentas {
+     string Vardas="A", Pavarde="B";
+   // int *paz;
+     vector<int> paz;
+    int egz;
+    double rez;
+    
+};
+void outputas(const vector<Studentas> &grupe);
+
+int main(){
+
+    Studentas A;
+    vector<Studentas> grupe;
+    cout<<"Kiek studentu yra grupeje:";
+    int m;
+    cin>>m; 
+    cout<<"galutinio pazymio skaiciavimui bus naudojamas: \n 1 - vidurkis \n 2 - mediana \n Pasirinkite: ";
+    int pasirinkimas;
+    cin>>pasirinkimas;
+    for(int ii=0;ii<m;ii++){
+    cout<<"Iveskite studento varda ir pavarde: ";
+    cin>>A.Vardas>>A.Pavarde;
+    cout<<"Iveskite semestro pazymius : \n Kiek pazymiu bus ivesta? ";
+    int n,temp,sum=0;
+     cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Iveskite "<<i+1<<" pazymi is "<<n<<": ";
+        cin>>temp;
+
+        A.paz.push_back(temp);
+        sum+=temp;
+    }
+    
+    cout<<"Iveskite egzamino pazymi: ";
+    cin>>A.egz;
+    if(pasirinkimas==1) A.rez=sum*1.0/(A.paz.size()*1.0)*0.4+A.egz*0.6;  
+    else {std::sort (A.paz.begin(),A.paz.end());
+if(A.paz.size()%2==1) A.rez= A.paz[A.paz.size()/2];
+else  A.rez=(A.paz[A.paz.size()/2-1]+A.paz[A.paz.size()/2])/2.0;}
+    grupe.push_back(A);
+    A.paz.clear();
+    }
+
+
+    outputas(grupe);
+    return 0;
+}
+
+
+void outputas(const vector<Studentas> &grupe)
+{for(auto A:grupe){
+    cout<<left<<setw(10)<<A.Vardas<<left<<setw(20)<<A.Pavarde<<endl;
+   // for(auto a:A.paz){cout<<setw(3)<<a;}
+    //cout<<setw(10)<<A.egz;
+    cout<<setw(20)<<fixed<<setprecision(2)<<A.rez<<endl;}}

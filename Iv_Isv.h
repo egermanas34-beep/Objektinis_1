05b91551e031b9c0b2673e7b452b3a3fbf2f1b64
egermanas34-beep <egermanas34@gmail.com>
2026-03-26 11:53:13 +0200

    Delete Iv_Isv.h

--- Iv_Isv.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "studentas.h"
-
-void inputas(Studentas &A, StudentuGrupe &grupe, int &pasirinkimas);
-void outputas(const StudentuGrupe &vargsiukai, const StudentuGrupe &smartukai, int &pasirinkimas, int &isvedimas, int &m);
-StudentuGrupe bufer_nusk(string &read_vardas,int &pasirinkimas, int &m);

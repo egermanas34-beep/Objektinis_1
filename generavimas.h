ac40e2c28213c70945d1609a84812268d65ab39b
egermanas34-beep <egermanas34@gmail.com>
2026-03-26 11:54:32 +0200

    Delete generavimas.h

--- generavimas.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "studentas.h"
-void generavimasSk(Studentas &A, StudentuGrupe &grupe, int &pasirinkimas);
-void generavimasVisko(Studentas &A, StudentuGrupe &grupe, int &pasirinkimas);
-void failuGeneravimas( int &n);
